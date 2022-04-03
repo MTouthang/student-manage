0d6d38a5c804d3ca47053de952cf20c0c029fcc2
Mamang touthang <huebart6@gmail.com>
2022-04-03 12:08:08 +0530

    jpa annotation

--- src/main/java/mang/io/studentmanage/entity/Student.java
+++ src/main/java/mang/io/studentmanage/entity/Student.java
@@ -6,23 +6,40 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(
+        name="tbl_student",
+        uniqueConstraints = @UniqueConstraint(
+                name="emailid_unique",
+                columnNames = "email_address"
+        )
+)
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(
+            name="email_address",
+            nullable = false
+    )
     private String emailId;
     private String guardianName;
     private String guardianEmail;

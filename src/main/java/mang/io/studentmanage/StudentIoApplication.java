6a699cd606436508c81866fb679eb814da0c7d05
Mamang touthang <huebart6@gmail.com>
2022-04-03 11:38:38 +0530

    Initial commit

---
+++ src/main/java/mang/io/studentmanage/StudentIoApplication.java
@@ -0,0 +1,13 @@
+package mang.io.studentmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentIoApplication.class, args);
+	}
+
+}

be08f61d64878eb129b68ed3ba08f1764c0a6f48
Mamang touthang <huebart6@gmail.com>
2022-04-03 17:26:35 +0530

    many to many with extra table

--- src/main/java/mang/io/studentmanage/repository/CourseRepository.java
+++ src/main/java/mang/io/studentmanage/repository/CourseRepository.java
@@ -1,7 +1,14 @@
 package mang.io.studentmanage.repository;
 
 import mang.io.studentmanage.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    Page<Course> findByTitleContaining(
+            String title,
+            Pageable pageable
+    );
 }
--- src/test/java/mang/io/studentmanage/repository/CourseRepositoryTest.java
+++ src/test/java/mang/io/studentmanage/repository/CourseRepositoryTest.java
@@ -1,14 +1,16 @@
 package mang.io.studentmanage.repository;
 
 import mang.io.studentmanage.entity.Course;
+import mang.io.studentmanage.entity.Student;
 import mang.io.studentmanage.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CourseRepositoryTest {
@@ -35,6 +37,87 @@ class CourseRepositoryTest {
                 .credit(7)
                 .teacher(teacher)
                 .build();
+
+        courseRepository.save(course);
+    }
+
+    @Test
+    // Pagination
+    public void findAllPagination() {
+        Pageable firstPageWithThreeRecords = PageRequest.of(0, 3);
+
+        Pageable secondPageWithThreeRecords = PageRequest.of(1, 3);
+
+        List<Course> courses =
+                courseRepository.findAll(firstPageWithThreeRecords).getContent();
+        long totalPages =
+                courseRepository.findAll(firstPageWithThreeRecords).getTotalPages();
+
+        System.out.println("Courses = " + courses);
+        System.out.println("total elements= " + totalPages);
+
+    }
+    // Sorting
+    @Test
+    public void findAllSorting(){
+        Pageable sortByTitle =
+                PageRequest.of(
+                        0,
+                        2,
+                        Sort.by("title")
+                );
+        Pageable sortByCreditDesc =
+                PageRequest.of(
+                        0,
+                        2,
+                        Sort.by("credit").descending()
+                );
+
+        Pageable sortByTitleAndCreditDesc = PageRequest.of(
+                0,
+                2,
+                Sort.by("title").descending()
+                        .and(Sort.by("credit"))
+        );
+        List<Course> courses
+                = courseRepository.findAll(sortByTitle).getContent();
+        System.out.println("Courses = "+ courses);
+    }
+
+    @Test
+    public void printfindByTitleContaining(){
+        Pageable firstPageTenRecords =
+                PageRequest.of(0, 10);
+
+        List<Course> courses =
+                courseRepository.findByTitleContaining(
+                        "D",
+                        firstPageTenRecords).getContent();
+        System.out.println("courses = " + courses );
+    }
+
+    @Test
+    public void saveCourseWithStudentAndTeacher(){
+
+        Teacher teacher = Teacher.builder()
+                .firstName("Pross")
+                .lastName("Newton")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("abi")
+                .lastName("Singh")
+                .emailId("abising@gmail.com")
+                .build();
+
+        Course course = Course
+                .builder()
+                .title("AI")
+                .credit(7)
+                .teacher(teacher)
+                .build();
+
+        course.addStudents(student);
 
         courseRepository.save(course);
     }
